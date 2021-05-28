9573fd85a93284c3483c7fcc045de629441608a1
하찬균 <gkcksrbs@ajou.ac.kr>
2021-05-29 03:08:14 +0900

    login/register

--- server/DB.js
+++ server/DB.js
@@ -1,20 +1,10 @@
 const pg = require('pg');
+const config = require('./secret');
 
-const config = {
-    user:'postgres',
-    host:'localhost',
-    database: 'SWEngineering',
-    password: 'hcg1208*',
-    port: '5433'
-}
-
-const pgsql = new pg.Client(config);
+const pgsql = new pg.Client(config.config);
 
 pgsql.connect(err => {
     if(err) throw err;
-    // else{~
-    //     queryDatabase();
-    // }
 })
 
 function queryDatabase(){
@@ -29,23 +19,21 @@ function queryDatabase(){
         rows.map(row => {
             console.log(`Read: ${JSON.stringify(row)}`);
         });
-
-        // process.exit();
     })
     .catch(err => {
         console.log(err);
     });
 }
 
-function findUser(email, password){
+async function findUser(email, password){
     const query = `
         select * from userInfo where email = '${email}' and pw = '${password}';
     `
     let rows;
-    pgsql.query(query)
+    await pgsql.query(query)
     .then(res => {
-         rows = res.rows;
-
+        rows = res.rows;
+        
         rows.map(row => {
             console.log(`Read: ${JSON.stringify(row)}`);
         });
@@ -53,15 +41,14 @@ function findUser(email, password){
     .catch(err => {
         console.log(err);
     })
-    return rows;
 }
 
-function insertUser(email, password, name){
+async function insertUser(email, password, name){
     const query = `
         insert into userInfo values ('${email}', '${password}', '${name}');
     `
 
-    pgsql.query(query)
+    await pgsql.query(query)
     .catch(err => {
         console.log("err: " + err);
     })
