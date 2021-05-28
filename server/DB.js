f32ec093cbaa340227471ae306973a3f3adf6256
하찬균 <gkcksrbs@ajou.ac.kr>
2021-05-29 00:08:48 +0900

    로그인

--- server/DB.js
+++ server/DB.js
@@ -12,9 +12,9 @@ const pgsql = new pg.Client(config);
 
 pgsql.connect(err => {
     if(err) throw err;
-    else{
-        queryDatabase();
-    }
+    // else{
+    //     queryDatabase();
+    // }
 })
 
 function queryDatabase(){
@@ -37,6 +37,28 @@ function queryDatabase(){
     });
 }
 
+function findUser(email, password){
+    const query = `
+        select * from userInfo where email = '${email}' and pw = '${password}';
+    `
+
+    pgsql.query(query)
+    .then(res => {
+        const rows = res.rows;
+
+        if(rows.length == 0){
+            console.log(0);
+        }
+
+        rows.map(row => {
+            console.log(`Read: ${JSON.stringify(row)}`);
+        });
+    })
+    .catch(err => {
+        console.log(err);
+    })
+}
+
 module.exports = {
-    pgsql
+    pgsql, queryDatabase, findUser
 }
