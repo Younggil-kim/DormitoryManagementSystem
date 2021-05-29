8d96235ba48b2e15c5e75c879e40204dbda94f79
younggil <gom991@ajou.ac.kr>
2021-05-30 03:12:24 +0900

    전체 게시물

---
+++ client/src/component/views/BoardPage/TotalBoard.js
@@ -0,0 +1,26 @@
+import { useEffect, useState} from 'react';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import ReactHtmlParser from 'react-html-parser';
+import Axios from 'axios';
+import React from 'react';
+import {withRouter} from 'react-router-dom';
+
+import {Button} from "react-bootstrap";
+
+function TotalBoard(){
+    
+    const [viewContent , setViewContent] = useState([]);
+
+    useEffect(()=>{
+      Axios.get('http://localhost:8000/api/get').then((response)=>{
+        setViewContent(response.data);
+        console.log(viewContent);
+      })
+    },[viewContent])
+
+}
+
+
+
+export default withRouter(TotalBoard)
